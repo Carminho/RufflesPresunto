49ac87a4992b1fad3967cf6c2a0b29c406410327
JMCMARQUES <jo.marques@live.com.pt>
2019-02-17 21:28:33 +0000

    Adding small modification at the ItemType class

--- src/org/academiadecodigo/bootcamp/Item/ItemType.java
+++ src/org/academiadecodigo/bootcamp/Item/ItemType.java
@@ -4,29 +4,28 @@ import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public enum ItemType {
 
-    DEAD_ET_BED(1,2,"bed.png"),
-    DESK(1,2,"bed.png"),
-    DRAWER(1,2,"bed.png"),
-    KEY(1,2,"bed.png"),
-    SAFE(1,2,"bed.png"),
-    CARPET(1,2,"bed.png"),
-    TABLE(1,2,"bed.png"),
-    CEO(1,2,"bed.png"),
-    DEADBODY(1,2,"bed.png"),
-    COMPUTER(1,2,"bed.png"),
-    ELVIS(1,2,"bed.png"),
-    BIKE(1,2,"bed.png"),
-    TLP(1,2,"bed.png"),
-    ET_GAME_DISK(1,2,"bed.png");
+    DEAD_ET_BED(1,2,1,2,"bed.png"),
+    DESK(1,2,1,2,"bed.png"),
+    DRAWER(1,2,1,2,"bed.png"),
+    KEY(1,2,1,2,"bed.png"),
+    SAFE(1,2,1,2,"bed.png"),
+    CARPET(1,2,1,2,"bed.png"),
+    TABLE(1,2,1,2,"bed.png"),
+    CEO(1,2,1,2,"bed.png"),
+    DEADBODY(1,2,1,2,"bed.png"),
+    COMPUTER(1,2,1,2,"bed.png"),
+    ELVIS(1,2,1,2,"bed.png"),
+    BIKE(1,2,1,2,"bed.png"),
+    TLP(1,2,1,2,"bed.png"),
+    ET_GAME_DISK(1,2,1,2,"bed.png");
 
 
 //PROPERTIES
     private Picture image;
 
 
-
 //CONSTRUCTOR
-    ItemType (double x, double y, String image){
+    ItemType (int col, int row, double x, double y, String image){
         this.image = new Picture(x,y,image);
     }
 
