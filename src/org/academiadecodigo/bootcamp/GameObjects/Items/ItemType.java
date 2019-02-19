1df35477ab077162d0c7c54f9fad38ced9c83745
João Marques <jo.marques@live.com.pt>
2019-02-19 20:24:22 +0000

    adding the changes at the itemType class, to include all positions at the first room

--- src/org/academiadecodigo/bootcamp/GameObjects/Items/ItemType.java
+++ src/org/academiadecodigo/bootcamp/GameObjects/Items/ItemType.java
@@ -6,20 +6,28 @@ import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public enum ItemType {
 
-    DEAD_ET_BED(1,2,1,2,"Note: the code position is somewhere in those drawers."),
-    DESK(1,2,1,2,"Code number: 7"),
-    DRAWER(1,2,1,2,"The code is at 1st position."),
-    KEY(1,2,1,2,"Hmm... where can I use this key?"),
-    SAFE(1,2,1,2,"Great. I have de code number, but in which position? Hmmm..."),
-    CARPET(1,2,1,2,"Code number: 4"),
-    TABLE(1,2,1,2,"Button opens the door."),
-    CEO(1,2,1,2,"Code number: 5"),
-    DEADBODY(1,2,1,2,"You will never find out the code number in 3th position. Only the CEO knew that."),
-    COMPUTER(1,2,1,2,"The scientist usually uses the number of objects in this room to represent the code number on 4th position."),
-    ELVIS(1,2,1,2,"Unknown"),
-    BIKE(1,2,1,2,"bed.png"),
-    TLP(1,2,1,2,"REBMUN EDOC EHT RETNE"),
-    ET_GAME_DISK(1,2,1,2,"Finally I'm going to get out of this hole in Alamogordo.");
+    DEAD_ET_BED1(3,1,"Note: the code position is somewhere in those drawers."),
+    DEAD_ET_BED2(3,2,"Note: the code position is somewhere in those drawers."),
+    DEAD_ET_BED3(3,3,"Note: the code position is somewhere in those drawers."),
+    DESK1(0,6,"Code number: 7"),
+    DESK2(0,7,"Code number: 7"),
+    DESK3(1,6,"Code number: 7"),
+    DESK4(1,7,"Code number: 7"),
+    DRAWER1(7,8,"The code is at 1st position."),
+    DRAWER2(7,9,"The code is at 1st position."),
+    DRAWER3(8,8,"The code is at 1st position."),
+    DRAWER4(8,9,"The code is at 1st position."),
+    KEY(1,2,"Hmm... where can I use this key?"),
+    SAFE(1,2,"Great. I have de code number, but in which position? Hmmm..."),
+    CARPET(1,2,"Code number: 4"),
+    TABLE(1,2,"Button opens the door."),
+    CEO(1,2,"Code number: 5"),
+    DEADBODY(1,2,"You will never find out the code number in 3th position. Only the CEO knew that."),
+    COMPUTER(1,2,"The scientist usually uses the number of objects in this room to represent the code number on 4th position."),
+    ELVIS(1,2,"Unknown"),
+    BIKE(1,2,"bed.png"),
+    TLP(1,2,"REBMUN EDOC EHT RETNE"),
+    ET_GAME_DISK(1,2,"Finally I'm going to get out of this hole in Alamogordo.");
 
 
 
@@ -30,8 +38,9 @@ public enum ItemType {
 
 
 //CONSTRUCTOR
-    ItemType (int col, int row, double x, double y, String message){
+    ItemType (int col, int row, String message){
         pos = new Position(col,row);
+        this.message=message;
     }
 
 
