f72710390798d97a5a7a8e7abdb9c299b0e26d10
ANAlogic <ana.raquel.carlos.carmo@hotmail.com>
2019-02-21 19:47:14 +0000

    new methods to keyboard events in Game class - less verbose code

--- src/org/academiadecodigo/bootcamp/Game.java
+++ src/org/academiadecodigo/bootcamp/Game.java
@@ -13,6 +13,7 @@ public class Game implements KeyboardHandler {
     //PROPERTIES
     private Room[] rooms;
     private Room currentRoom;
+    private Keyboard kb;
 
 
 
@@ -22,13 +23,14 @@ public class Game implements KeyboardHandler {
     }
 
 
+
     //METHODS
     public void start() {
-        controlKeys();
         createRooms();
         currentRoom = new Room(RoomType.DISSECTION_CELL);
-
+        createControlKeys();
     }
+
 
 
     public void setCurrentRoom(Room currentRoom) {
@@ -44,85 +46,35 @@ public class Game implements KeyboardHandler {
     }
 
 
-    public void controlKeys() {
-        Keyboard kb = new Keyboard(this);
+    public void createControlKeys () {
+        kb = new Keyboard(this);
 
-        KeyboardEvent leftPressed = new KeyboardEvent();
-        leftPressed.setKey(KeyboardEvent.KEY_LEFT);
-        leftPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        kb.addEventListener(leftPressed);
-
-        KeyboardEvent rightPressed = new KeyboardEvent();
-        rightPressed.setKey(KeyboardEvent.KEY_RIGHT);
-        rightPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        kb.addEventListener(rightPressed);
-
-        KeyboardEvent upPressed = new KeyboardEvent();
-        upPressed.setKey(KeyboardEvent.KEY_UP);
-        upPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        kb.addEventListener(upPressed);
-
-        KeyboardEvent downPressed = new KeyboardEvent();
-        downPressed.setKey(KeyboardEvent.KEY_DOWN);
-        downPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        kb.addEventListener(downPressed);
-
-        KeyboardEvent spacePressed = new KeyboardEvent();
-        spacePressed.setKey(KeyboardEvent.KEY_SPACE);
-        spacePressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        kb.addEventListener(spacePressed);
-
-        KeyboardEvent zeroPressed = new KeyboardEvent();
-        zeroPressed.setKey(KeyboardEvent.KEY_0);
-        zeroPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        kb.addEventListener(zeroPressed);
-
-        KeyboardEvent onePressed = new KeyboardEvent();
-        onePressed.setKey(KeyboardEvent.KEY_1);
-        onePressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        kb.addEventListener(onePressed);
-
-        KeyboardEvent twoPressed = new KeyboardEvent();
-        twoPressed.setKey(KeyboardEvent.KEY_2);
-        twoPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        kb.addEventListener(twoPressed);
-
-        KeyboardEvent threePressed = new KeyboardEvent();
-        threePressed.setKey(KeyboardEvent.KEY_3);
-        threePressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        kb.addEventListener(threePressed);
-
-        KeyboardEvent fourPressed = new KeyboardEvent();
-        fourPressed.setKey(KeyboardEvent.KEY_4);
-        fourPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        kb.addEventListener(fourPressed);
-
-        KeyboardEvent fivePressed = new KeyboardEvent();
-        fivePressed.setKey(KeyboardEvent.KEY_5);
-        fivePressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        kb.addEventListener(fivePressed);
-
-        KeyboardEvent sixPressed = new KeyboardEvent();
-        sixPressed.setKey(KeyboardEvent.KEY_6);
-        sixPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        kb.addEventListener(sixPressed);
-
-        KeyboardEvent sevenPressed = new KeyboardEvent();
-        sevenPressed.setKey(KeyboardEvent.KEY_7);
-        sevenPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        kb.addEventListener(sevenPressed);
-
-        KeyboardEvent eightPressed = new KeyboardEvent();
-        eightPressed.setKey(KeyboardEvent.KEY_8);
-        eightPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        kb.addEventListener(eightPressed);
-
-        KeyboardEvent ninePressed = new KeyboardEvent();
-        ninePressed.setKey(KeyboardEvent.KEY_9);
-        ninePressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        kb.addEventListener(ninePressed);
-
+        addEvent(KeyboardEvent.KEY_LEFT, KeyboardEventType.KEY_PRESSED);
+        addEvent(KeyboardEvent.KEY_RIGHT, KeyboardEventType.KEY_PRESSED);
+        addEvent(KeyboardEvent.KEY_UP, KeyboardEventType.KEY_PRESSED);
+        addEvent(KeyboardEvent.KEY_DOWN, KeyboardEventType.KEY_PRESSED);
+        addEvent(KeyboardEvent.KEY_SPACE, KeyboardEventType.KEY_PRESSED);
+        addEvent(KeyboardEvent.KEY_0, KeyboardEventType.KEY_PRESSED);
+        addEvent(KeyboardEvent.KEY_1, KeyboardEventType.KEY_PRESSED);
+        addEvent(KeyboardEvent.KEY_2, KeyboardEventType.KEY_PRESSED);
+        addEvent(KeyboardEvent.KEY_3, KeyboardEventType.KEY_PRESSED);
+        addEvent(KeyboardEvent.KEY_4, KeyboardEventType.KEY_PRESSED);
+        addEvent(KeyboardEvent.KEY_5, KeyboardEventType.KEY_PRESSED);
+        addEvent(KeyboardEvent.KEY_6, KeyboardEventType.KEY_PRESSED);
+        addEvent(KeyboardEvent.KEY_7, KeyboardEventType.KEY_PRESSED);
+        addEvent(KeyboardEvent.KEY_8, KeyboardEventType.KEY_PRESSED);
+        addEvent(KeyboardEvent.KEY_9, KeyboardEventType.KEY_PRESSED);
     }
+
+
+
+    public void addEvent (int key,KeyboardEventType type){
+        KeyboardEvent event = new KeyboardEvent();
+        event.setKey(key);
+        event.setKeyboardEventType(type);
+        kb.addEventListener(event);
+    }
+
 
 
     @Override
