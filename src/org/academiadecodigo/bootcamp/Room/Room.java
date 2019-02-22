8556c50208856587f541613cba16a8b1dc19c6b3
ANAlogic <ana.raquel.carlos.carmo@hotmail.com>
2019-02-22 17:27:02 +0000

    1st scenery image added

--- src/org/academiadecodigo/bootcamp/Room/Room.java
+++ src/org/academiadecodigo/bootcamp/Room/Room.java
@@ -18,12 +18,8 @@ public class Room {
     private GameObject[] items;
     private RoomType type;
     private Picture scenery;
-    private Rectangle box;
-    private Text text;
     private boolean isShowing;
     private Picture picture;
-
-
 
 
     //CONSTRUCTOR
@@ -33,8 +29,6 @@ public class Room {
         pos = new Position(Position.PADDING, Position.PADDING);
 
         items = new GameObject[type.getDoors().length + type.getItems().length];
-
-        box = new Rectangle(2 * pos.CELL_SIZE, 3 * pos.CELL_SIZE, 6 * pos.CELL_SIZE, 4 * pos.CELL_SIZE);
 
         scenery = new Picture(pos.PADDING, pos.PADDING, "dissection-room.jpeg");
         scenery.draw();
@@ -52,7 +46,6 @@ public class Room {
             System.out.println("Index: " + index + " Item: " + items[index]);
             index++;
         }
-
 
 
         et = new ET(6, 2);
@@ -103,44 +96,8 @@ public class Room {
     }
 
 
-    private void showTextBox () {
-        box.fill();
-        box.setColor(Color.WHITE);
-        text.draw();
-        text.setColor(Color.BLACK);
-    }
-
-    private void hideTextBox () {
-        box.delete();
-        text.delete();
-    }
-
 }
 
 
 
-    /*
-        public void interact () {
-        for (int i = 0; i < items.length; i++) {
-            System.out.println("Interacting");
-            System.out.println("ET position: " + et.getPos());
-            System.out.println("item " + i + " position :" + items[i].getPos());
-            if (!isShowing) {
-
-                if (et.getPos().equals(items[i].getPos())) {
-                    items[i].showInteraction(items[i], this.isShowing);
-                    this.isShowing = true;
-                    System.out.println("Interact show. Boolean: " + this.isShowing );
-                    return;
-                }
-            } else {
-                items[i].showInteraction(items[i], this.isShowing);
-                this.isShowing = false;
-                System.out.println("interact delete. Boolean: " + this.isShowing );
-                return;
-            }
-        }
-
-    }
-}
 
