f24ee24e2f624dd9158bee28e784e6ff1443ed67
ANAlogic <ana.raquel.carlos.carmo@hotmail.com>
2019-02-24 13:33:25 +0000

    code numbers in individual positions

--- src/org/academiadecodigo/bootcamp/Game.java
+++ src/org/academiadecodigo/bootcamp/Game.java
@@ -103,7 +103,8 @@ public class Game implements KeyboardHandler {
                 break;
             case KeyboardEvent.KEY_D:
                 if (currentRoom.getType() == RoomType.WAREHOUSE && comparePosition(teleporter)) {
-                    teleporter.deleteLastDigit();
+                    //teleporter.deleteLastDigit();
+                    teleporter.deleteAll();
                     System.out.println(teleporter.getUserCode());
                 }
                 break;
@@ -154,15 +155,22 @@ public class Game implements KeyboardHandler {
 
     public void interact() {
         for (int i = 0; i < currentRoom.getItems().length; i++) {
+            System.out.println("current room " + currentRoom);
+            System.out.println("is showing message? " + currentRoom.isShowing());
+            System.out.println(currentRoom.getPicture());
 
             if (currentRoom.isShowing()) {                                                          //if message is showing
                 if (currentRoom.getPicture() == null) {
                     box.delete();
                     text.delete();
                     currentRoom.setIsShowing(false);
+                    System.out.println("apagar caixa de texto");
                 } else {
                     currentRoom.getPicture().delete();
+                    box.delete();
+                    text.delete();
                     currentRoom.setIsShowing(false);
+                    System.out.println("apagar imagem");
                 }
                 return;
             }
@@ -172,6 +180,7 @@ public class Game implements KeyboardHandler {
 
                 if (currentRoom.getItems()[i].getClass() == Door.class) {                           //if interacting with a door (change room and creates new ET in new position)
                     changeRoom(i);
+                    System.out.println("mudar de room");
                     return;
                 }
 
@@ -179,6 +188,7 @@ public class Game implements KeyboardHandler {
                     if (((Item) currentRoom.getItems()[i]).getType().equals(ItemType.KEY)) {       //if item is a key.
                         ((Item) currentRoom.getItems()[i]).getType().setFound();                   //if we interact with key, set key as found.
                         showMessage(i);
+                        System.out.println("item is a key");
                         return;
                     }
                     if ((((Item) currentRoom.getItems()[i]).getType().equals(ItemType.SAFE1) || ((Item) currentRoom.getItems()[i]).getType().equals(ItemType.SAFE2))      //if item is a safe
@@ -187,9 +197,11 @@ public class Game implements KeyboardHandler {
                         box.setColor(Color.WHITE);
                         text.draw();
                         currentRoom.setIsShowing(true);
+                        System.out.println("item is a safe");
                         return;
                     } else {
                         showMessage(i);                                                                //if any other item
+                        System.out.println("other item");
                         return;
                     }
                 }
@@ -205,6 +217,7 @@ public class Game implements KeyboardHandler {
             currentRoom = new Room(RoomType.EGGXIT);
             et = new ET(0,0);
             et.show();
+            System.out.println("change to eggxit room");
         }
 
     }
--- src/org/academiadecodigo/bootcamp/GameObjects/Items/Teleporter.java
+++ src/org/academiadecodigo/bootcamp/GameObjects/Items/Teleporter.java
@@ -11,57 +11,141 @@ public class Teleporter {
     private final String EGGXIT_CODE = "93577";
     private String userCode;
     private Position[] positions;
-    private Text code;
+    //private Text code;
+    private Picture[] codeAna;
     private boolean eggxitON;
 
 
     // CONSTRUCTOR
     public Teleporter() {
         userCode = "";
-        code = new Text(5 * Position.CELL_SIZE, 4 * Position.CELL_SIZE, userCode);
-        positions = new Position[] {ItemType.TLP1.getPos(), ItemType.TLP2.getPos(), ItemType.TLP3.getPos(), ItemType.TLP4.getPos(), ItemType.TLP5.getPos(), ItemType.TLP6.getPos()};
+        codeAna = new Picture[5];
+        //code = new Text(5 * Position.CELL_SIZE, 4 * Position.CELL_SIZE, userCode);
+        positions = new Position[]{ItemType.TLP1.getPos(), ItemType.TLP2.getPos(), ItemType.TLP3.getPos(), ItemType.TLP4.getPos(), ItemType.TLP5.getPos(), ItemType.TLP6.getPos()};
     }
 
     // METHODS
     public void enterCode(String number) {
-        userCode = userCode + number;
-        code.delete();
+        if (userCode.length() < 5) {
+            userCode = userCode + number;
+
+            if (userCode.length() == 1) {
+                usercodePosition(0, 2);
+            }
+            if (userCode.length() == 2) {
+                usercodePosition(1, 3.4);
+            }
+            if (userCode.length() == 3) {
+                usercodePosition(2, 4.75);
+            }
+            if (userCode.length() == 4) {
+                usercodePosition(3, 6.2);
+            }
+            if (userCode.length() == 5) {
+                usercodePosition(4, 7.5);
+            }
+        }
+
+        /*code.delete();
         code = new Text(5 * Position.CELL_SIZE, 4 * Position.CELL_SIZE, userCode);
-        code.grow(40,40);
-        code.draw();
-    }
+        code.grow(40, 40);
+        code.draw();*/
 
-    public void verifyCode() {
-        if (userCode.equals(RIGHT_CODE)) {                               // Win game
-            Picture win = new Picture(Position.PADDING, Position.PADDING, "resources/win.png");
-            win.draw();
-        } else if (userCode.equals(EGGXIT_CODE)) {                       // Go to easter egg
-            eggxitON = true;
-            System.out.println("Go to easter egg");
-        } else {                                                    // Wrong code, try again
-            System.out.println("Wrong code, try again");
+        }
+
+
+        private void usercodePosition ( int number, double col){
+            switch (userCode.charAt(number)) {
+                case '0':
+                    codeAna[number] = new Picture(col * Position.CELL_SIZE, 4.9 * Position.CELL_SIZE, "resources/numbers/0.png");
+                    codeAna[number].draw();
+                    break;
+                case '1':
+                    codeAna[number] = new Picture(col * Position.CELL_SIZE, 4.9 * Position.CELL_SIZE, "resources/numbers/1.png");
+                    codeAna[number].draw();
+                    break;
+                case '2':
+                    codeAna[number] = new Picture(col * Position.CELL_SIZE, 4.9 * Position.CELL_SIZE, "resources/numbers/2.png");
+                    codeAna[number].draw();
+                    break;
+                case '3':
+                    codeAna[number] = new Picture(col * Position.CELL_SIZE, 4.9 * Position.CELL_SIZE, "resources/numbers/3.png");
+                    codeAna[number].draw();
+                    break;
+                case '4':
+                    codeAna[number] = new Picture(col * Position.CELL_SIZE, 4.9 * Position.CELL_SIZE, "resources/numbers/4.png");
+                    codeAna[number].draw();
+                    break;
+                case '5':
+                    codeAna[number] = new Picture(col * Position.CELL_SIZE, 4.9 * Position.CELL_SIZE, "resources/numbers/5.png");
+                    codeAna[number].draw();
+                    break;
+                case '6':
+                    codeAna[number] = new Picture(col * Position.CELL_SIZE, 4.9 * Position.CELL_SIZE, "resources/numbers/6.png");
+                    codeAna[number].draw();
+                    break;
+                case '7':
+                    codeAna[number] = new Picture(col * Position.CELL_SIZE, 4.9 * Position.CELL_SIZE, "resources/numbers/7.png");
+                    codeAna[number].draw();
+                    break;
+                case '8':
+                    codeAna[number] = new Picture(col * Position.CELL_SIZE, 4.9 * Position.CELL_SIZE, "resources/numbers/8.png");
+                    codeAna[number].draw();
+                    break;
+                case '9':
+                    codeAna[number] = new Picture(col * Position.CELL_SIZE, 4.9 * Position.CELL_SIZE, "resources/numbers/9.png");
+                    codeAna[number].draw();
+                    break;
+                default:
+                    System.out.println("You must insert a number!");
+            }
+        }
+
+
+        public void verifyCode () {
+            if (userCode.equals(RIGHT_CODE)) {                               // Win game
+                Picture win = new Picture(Position.PADDING, Position.PADDING, "resources/win.png");
+                win.draw();
+            } else if (userCode.equals(EGGXIT_CODE)) {                       // Go to easter egg
+                eggxitON = true;
+                System.out.println("Go to easter egg");
+            } else {                                                    // Wrong code, try again
+                System.out.println("Wrong code, try again");
+            }
+        }
+
+
+        public void deleteAll () {
+            userCode = "";
+            int j = 0;
+            for (Picture pic : codeAna) {
+                if (pic != null) {
+                    codeAna[j].delete();
+                    j++;
+                }
+            }
+        }
+
+        /*public void deleteLastDigit () {
+            if (userCode.length() > 0) {
+                userCode = userCode.substring(0, userCode.length() - 1);
+                code.delete();
+                code = new Text(5 * Position.CELL_SIZE, 4 * Position.CELL_SIZE, userCode);
+                code.grow(40, 40);
+                code.draw();
+            }
+        }*/
+
+
+        public Position[] getPositions () {
+            return positions;
+        }
+
+        public String getUserCode () {
+            return userCode;
+        }
+
+        public boolean isEggxitON () {
+            return eggxitON;
         }
     }
-
-    public void deleteLastDigit() {
-        if (userCode.length() > 0) {
-            userCode = userCode.substring(0, userCode.length() - 1);
-            code.delete();
-            code = new Text(5 * Position.CELL_SIZE, 4 * Position.CELL_SIZE, userCode);
-            code.grow(40,40);
-            code.draw();
-        }
-    }
-
-    public Position[] getPositions() {
-        return positions;
-    }
-
-    public String getUserCode() {
-        return userCode;
-    }
-
-    public boolean isEggxitON (){
-        return eggxitON;
-    }
-}
